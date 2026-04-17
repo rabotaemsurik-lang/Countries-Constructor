e642d6d1e9c2c00132e33a02f66530ce32ba9aea
DenisHrytsai242 <ipz242_gdv@student.ztu.edu.ua>
2026-04-17 13:03:45 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function countryConstructor(title, capital, population, area){
+    this.title = title;
+    this.capital = capital;
+    this.population = population;
+    this.area = area;
+    this.showCountry = function (){
+        return `Країна: ${this.title}, столиця: ${this.capital}, населення: ${this.population}, area: ${this.area}`;
+    }
+}
+const countryFirst = new countryConstructor("Україна", "Київ", 35000000, 603700);
+const countrySecond = new countryConstructor("Франція", "Париж", 66700000, 643801);
+function showInfo(country){
+    for (let key in country){
+        if (typeof country[key] !== "function") {
+            console.log(`${key}: ${country[key]}`);
+        }
+    }
+
+}
+
+
+console.log(countryFirst.showCountry());
+showInfo(countryFirst);
+
+console.log("----------------");
+
+console.log(countrySecond.showCountry());
+showInfo(countrySecond);
